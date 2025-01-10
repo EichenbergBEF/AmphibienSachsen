557953d9d9eeadc5bf066adfe2bc464841e58e34
David Eichenberg <david.eichenberg@gmx.de>
2025-01-10 17:42:42 +0100

    Add files via upload
    Funktion zur Modellselektion, die so geschrieben wurde, dass die Modelloptimierung anhand des DIC immer auf Modellen beruht, die den räumlichen und zeitlichen Term im Modell belassen.

---
+++ Funktion_Modellselektion.R
@@ -0,0 +1,199 @@
+#### Funktion zur Modellselektion
+
+### Definieren einer Funktion zur Modellselektion.
+## Sie beruht auf der Funktion INLAModelSel' von gfalbery 
+## (https://rdrr.io/github/gfalbery/ggregplot/src/R/INLA%20Model%20Selection.R)
+## Modifikationen: Die Modelle wurden so angepasst, dass immer ein RW1 für den temporalen Trend
+## und immer ein spde für einen räumlichen Trend im Modell verbleiben.
+# Damit wird gewährleistet, dass alle Modelle auch zeitlich und räumlich explizit bleiben
+## und diese Modellkomponenten nicht aus dem Modell selektiert werden.
+## Andere, Parameter werden in einer rückwärtsgerichteten Modellselsktion 
+## iterativ aussortiert. Das optimale Modell wird anhand des DIC Kriteriums selektiert.
+
+###############################################################################
+INLAModelSel_amphi<- function (Response, Explanatory, Random = NULL, RandomModel = NULL, 
+                               Family, Data, Delta = 2, ScaleVariables = T, AllModels = F, 
+                               FullModelReturn = F, ReturnData = T, Beep = T, ...) 
+{
+  require(INLA)
+  require(ggplot2)
+  require(magrittr)
+  Data %<>% as.data.frame %>% mutate_if(is.character, as.factor)
+  if (ScaleVariables) {
+    Classes <- Data %>% dplyr::select(intersect(Explanatory, 
+                                                colnames(Data))) %>% sapply(class)
+    ToScale <- names(Classes[Classes %in% c("integer", "numeric")])
+    Data[, paste0(ToScale, ".Original")] <- Data[, ToScale]
+    Data %<>% mutate_at(ToScale, ~c(scale(.x)))
+    if (Family == "gaussian") {
+      Data[, paste0(Response, ".Original")] <- Data[, 
+                                                    Response]
+      Data %<>% mutate_at(Response, ~c(scale(.x)))
+    }
+  }
+  Explanatory2 <- paste(Explanatory, collapse = " + ")
+  if (!is.null(Random)) {
+    Random2 <- paste(paste0("f(", Random, ", model = '", 
+                            RandomModel, "')", "+ f(w, model = spde_amphi)"), collapse = " + ")
+    f1 <- as.formula(paste0("y", " ~ ", paste("-1 + Intercept + ",Explanatory2, 
+                                              " + ", Random2, collapse = " + ")))
+  }
+  else {
+    f1 <- as.formula(paste0(Response, " ~ ", paste("-1 + Intercept + ", Explanatory2, 
+                                                   collapse = " + ")))
+  }
+  if(!is.null(Random)) {
+    FullModel <- inla(f1, 
+                      family = Family, 
+                      
+                      data = inla.stack.data(Stack_amphi),
+                      control.predictor = list(A = inla.stack.A((Stack_amphi)),link =1),
+                      control.compute = list(dic = TRUE),
+                      verbose= F)
+  }
+  else{
+    FullModel <- inla(f1, 
+                      family = Family, 
+                      data = Data, 
+                      control.compute = list(dic = TRUE), 
+                      control.predictor = list(link =1))
+  }
+  ModelList <- AllModelList <- RemovedList <- FullFormulaList <- FormulaList <- list()
+  DICList <- dDICList <- list()
+  DICList[[1]] <- FullModel$dic$dic
+  FullFormulaList[[1]] <- f1
+  for (x in 1:length(Explanatory)) {
+    Explanatory3 <- paste(Explanatory[-x], collapse = " + ")
+    if (!is.null(Random)) {
+      f2 <- as.formula(paste0("y", " ~ ", paste("-1 + Intercept + ", Explanatory3, 
+                                                " + ", Random2, collapse = " + ")))
+    }
+    else {
+      f2 <- as.formula(paste0(Response, " ~ ", paste("-1 + Intercept + ", Explanatory3, 
+                                                     collapse = " + ")))
+    }
+    if(!is.null(Random)) {
+      Model1 <- inla(f2, 
+                     family = Family, 
+                     data = inla.stack.data(Stack_amphi),
+                     control.predictor = list(A = inla.stack.A((Stack_amphi)),link =1),
+                     control.compute = list(dic = TRUE),
+                     verbose= F)
+    }
+    else{
+      Model1 <- inla(f2, 
+                     family = Family, 
+                     data = Data, 
+                     control.compute = list(dic = TRUE), 
+                     control.predictor = list(link =1))
+    }
+    ModelList[[x]] <- Model1
+    FormulaList[[x]] <- f2
+  }
+  DICList[[2]] <- sapply(ModelList, function(y) y$dic$dic)
+  names(DICList[[2]]) <- Explanatory
+  dDICList[[1]] <- DICList[[2]] - DICList[[1]]
+  names(dDICList[[1]]) <- RemovedList[[1]] <- Explanatory
+  FullFormulaList[[2]] <- FormulaList
+  AllModelList[[1]] <- FullModel
+  AllModelList[[2]] <- ModelList
+  NewExplanatory <- Explanatory
+  while ((min(last(dDICList)) < Delta) & (length(NewExplanatory) > 
+                                          0)) {
+    print(paste("Losing", names(last(dDICList))[which(last(dDICList) == 
+                                                        min(last(dDICList)))]))
+    print(Text <- paste("Run", length(DICList)))
+    ModelList <- FormulaList <- list()
+    NewExplanatory <- NewExplanatory[-which(last(dDICList) == 
+                                              min(last(dDICList)))]
+    if (length(NewExplanatory) > 0) {
+      for (x in 1:length(NewExplanatory)) {
+        Explanatory3 <- paste(NewExplanatory[-x], collapse = " + ")
+        if (!is.null(Random)) {
+          f2 <- as.formula(paste0("y", " ~ ", paste("-1 + Intercept + ",Explanatory3, 
+                                                    " + ", Random2, collapse = " + ")))
+        }
+        else {
+          f2 <- as.formula(paste0(Response, " ~ ", paste("-1 + Intercept + ", Explanatory3, 
+                                                         collapse = " + ")))
+        }
+        if(!is.null(Random)) {
+          Model1 <- inla(f2, 
+                         family = Family, 
+                         data = inla.stack.data(Stack_amphi),
+                         control.predictor = list(A = inla.stack.A((Stack_amphi)),link =1),
+                         control.compute = list(dic = TRUE),
+                         verbose= F)
+        }
+        else{
+          Model1 <- inla(f2, family = Family, data = Data,
+                         control.predictor = list(link =1),
+                         control.compute = list(dic = TRUE),
+                         verbose= F)
+        }
+        ModelList[[x]] <- Model1
+        FormulaList[[x]] <- f2
+      }
+    }
+    else {
+      if (!is.null(Random)) {
+        f2 <- as.formula(paste0("y", " ~ ", paste("-1 + Intercept + ", Explanatory3, 
+                                                  " + ", Random2, collapse = " + ")))
+      }
+      else {
+        f2 <- as.formula(paste0(Response, " ~ ", paste("-1 + Intercept + ", Explanatory3, 
+                                                       collapse = " + ")))
+      }
+      if(!is.null(Random)) {
+        Model1 <- inla(f2, 
+                       family = Family, 
+                       data = inla.stack.data(Stack_amphi),
+                       control.predictor = list(A = inla.stack.A((Stack_amphi)),link =1),
+                       control.compute = list(dic = TRUE),
+                       verbose= F)
+      }
+      else{
+        Model1 <- inla(f2, family = Family, data = Data,
+                       control.predictor = list(link =1),
+                       control.compute = list(dic = TRUE),
+                       verbose= F)
+      }
+      ModelList[[x]] <- Model1
+      FormulaList[[x]] <- f2
+    }
+    AllModelList[[length(AllModelList) + 1]] <- ModelList
+    FullFormulaList[[length(FullFormulaList) + 1]] <- FormulaList
+    DICList[[length(DICList) + 1]] <- sapply(ModelList, 
+                                             function(y) y$dic$dic)
+    names(DICList[[length(DICList)]]) <- NewExplanatory
+    dDICList[[length(dDICList) + 1]] <- DICList[[length(DICList)]] - 
+      min(DICList[[length(DICList) - 1]])
+    names(dDICList[[length(dDICList)]]) <- RemovedList[[length(RemovedList) + 
+                                                          1]] <- NewExplanatory
+  }
+  FinalModel <- AllModelList[[length(AllModelList) - 1]][[which(dDICList[[length(dDICList) - 
+                                                                            1]] == min(dDICList[[length(dDICList) - 1]]))]]
+  FinalFormula <- FullFormulaList[[length(AllModelList) - 
+                                     1]][[which(dDICList[[length(dDICList) - 1]] == min(dDICList[[length(dDICList) - 
+                                                                                                    1]]))]]
+  ReturnList <- list(FinalModel = FinalModel, AllModels = AllModelList, 
+                     Removed = RemovedList, DIC = DICList, dDIC = dDICList, 
+                     FormulaList = FullFormulaList, FinalFormula = FinalFormula)
+  if (AllModels) {
+    ReturnList$AllModels <- AllModelList
+  }
+  if (FullModelReturn) {
+    ReturnList$FullModel <- FullModel
+  }
+  if (ReturnData) {
+    ReturnList$Data <- Data
+  }
+  if (Beep) 
+    beepr::beep()
+  return(ReturnList)
+}
+################################################################################
+
+
+###Funktion zur Standardisierung von daten
+MyStd <- function(x) { (x - mean(x)) / sd(x)}
